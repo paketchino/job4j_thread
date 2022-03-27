ea6ef4c05cf89aa5570dfbcd7ed22ce5cb29ec32
paketchino <chansgorn@gmail.com>
2022-03-27 23:03:57 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent;
+
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        System.out.println("Starting loading......");
+                        for (int i = 0; i < 100; i++) {
+                            System.out.print("\rLoading : " + i + "%");
+                            Thread.sleep(1000);
+                            System.out.println("\r");
+                            System.out.println(Thread.currentThread().getState());
+                            System.out.println("\r");
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
