912fd515196c892f17ed30b8b7b588e9a32baa1f
paketchino <chansgorn@gmail.com>
2022-03-29 00:14:11 +0300

    3. Прерывание нити [#1019 #280816] fix

--- src/main/java/ru/job4j/concurrent/ConsoleProgress.java
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -1,5 +1,9 @@
 package ru.job4j.concurrent;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ConsoleProgress implements Runnable {
 
     public static void main(String[] args) throws InterruptedException {
@@ -13,12 +17,14 @@ public class ConsoleProgress implements Runnable {
     public void run() {
         while (!Thread.currentThread().isInterrupted()) {
             try {
-                Thread.sleep(500);
-                System.out.print("\r load: " + "\\");
-                Thread.sleep(500);
-                System.out.print("\r load: " + "|");
-                Thread.sleep(500);
-                System.out.print("\r load: " + "/");
+                HashMap<Integer, String> str = new HashMap<>();
+                str.put(1, "\\");
+                str.put(2, "|");
+                str.put(3, "/");
+                for (Map.Entry entry : str.entrySet()) {
+                    Thread.sleep(500);
+                    System.out.print("\r load: " + entry.getValue());
+                }
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
