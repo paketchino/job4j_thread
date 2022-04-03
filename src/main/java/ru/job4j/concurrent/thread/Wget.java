19fc9ad7335f11953618c24df959f68a3df6f550
paketchino <chansgorn@gmail.com>
2022-04-03 20:24:30 +0300

    4. Скачивание файла с ограничением. [#144271 #281683]

--- src/main/java/ru/job4j/concurrent/thread/Wget.java
+++ src/main/java/ru/job4j/concurrent/thread/Wget.java
@@ -3,6 +3,7 @@ package ru.job4j.concurrent.thread;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Wget implements Runnable {
@@ -17,41 +18,45 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream(url)) {
-            byte[] dateBuffer = new byte[1024];
-            int bytesRead;
-            int downloadData = 0;
-            long begin = System.currentTimeMillis();
-            while ((bytesRead = in.read(dateBuffer, 0, 1024)) != -1) {
-                downloadData += bytesRead;
-                if (downloadData >= speed) {
-                    long finish = System.currentTimeMillis();
-                    if ((finish - begin) < 1000) {
-                        Thread.sleep(1000 - (finish - begin));
+        URL urlRef = null;
+        try {
+            urlRef = new URL(url);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        try {
+            assert urlRef != null;
+            try (BufferedInputStream in = new BufferedInputStream(urlRef.openStream());
+                     FileOutputStream fileOutputStream = new FileOutputStream(url)) {
+                byte[] dateBuffer = new byte[1024];
+                int bytesRead;
+                int downloadData = 0;
+                long begin = System.currentTimeMillis();
+                while ((bytesRead = in.read(dateBuffer, 0, 1024)) != -1) {
+                    downloadData += bytesRead;
+                    if (downloadData >= speed) {
+                        long finish = System.currentTimeMillis();
+                        if ((finish - begin) < 1000) {
+                            Thread.sleep(1000 - (finish - begin));
+                        }
+                        downloadData = 0;
+                        begin = System.currentTimeMillis();
                     }
-                    downloadData = 0;
-                    begin = System.currentTimeMillis();
+                    fileOutputStream.write(dateBuffer, 0, downloadData);
                 }
-                fileOutputStream.write(dateBuffer, 0, downloadData);
             }
-
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = 1048576;
-        Thread wget = new Thread(new Wget(url, speed));
+        int speed = Integer.parseInt("1048056");
+        Thread wget = new Thread(new Wget(args[0], speed));
         wget.start();
         wget.join();
         if (args[0].isEmpty()) {
             throw new IllegalArgumentException("Вы не ввели ссылку для скачивания");
-        }
-        if (args[1].isEmpty()) {
-            throw new IllegalArgumentException("Вы не ввели скорость скачивания");
         }
     }
 }
