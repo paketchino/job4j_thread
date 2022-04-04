4b87d5cc425b058ea96a673bca14025697d651b4
paketchino <chansgorn@gmail.com>
2022-04-04 23:37:06 +0300

    4. Thread без общих ресурсов [#267919] fix

--- src/main/java/ru/job4j/concurrent/User.java
+++ src/main/java/ru/job4j/concurrent/User.java
@@ -5,7 +5,7 @@ public class User {
     private int id;
     private String name;
 
-    public static User of (String name) {
+    public static User of(String name) {
         User user = new User();
         user.name = name;
         return user;
