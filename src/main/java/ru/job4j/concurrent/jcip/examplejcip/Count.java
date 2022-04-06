49b744a7d8bddc89e74b984383b8543d33e19f08
paketchino <chansgorn@gmail.com>
2022-04-06 18:06:52 +0300

    2. JCIP. Настройка библиотеки [#268575 #285069]

--- src/main/java/ru/job4j/concurrent/jcip/examplejcip/Count.java
+++ src/main/java/ru/job4j/concurrent/jcip/examplejcip/Count.java
@@ -1,7 +1,12 @@
-package ru.job4j.concurrent;
+package ru.job4j.concurrent.jcip.examplejcip;
 
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
 public class Count {
 
+    @GuardedBy("this")
     private int value;
 
     public synchronized void increment() {
