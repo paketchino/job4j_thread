c52cd6507e8f7e62ba2c748cfa4332125c2c061c
paketchino <chansgorn@gmail.com>
2022-04-16 01:02:12 +0300

    2. ExecutorService рассылка почты. [#63097] fix

---
+++ src/main/java/ru/job4j/concurrent/executorservice/User.java
@@ -0,0 +1,24 @@
+package ru.job4j.concurrent.executorservice;
+
+public class User {
+
+    private String username;
+    private final String email;
+
+    public User(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
