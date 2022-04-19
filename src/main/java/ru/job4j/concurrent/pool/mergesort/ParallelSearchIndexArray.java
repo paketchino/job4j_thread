d55359b8e4ea0ac6e0136fe4f8d3fb971b8d36da
paketchino <chansgorn@gmail.com>
2022-04-20 00:36:31 +0300

    3. ForkJoinPool [#315067 #290213]

--- src/main/java/ru/job4j/concurrent/pool/mergesort/ParallelSearchIndexArray.java
+++ src/main/java/ru/job4j/concurrent/pool/mergesort/ParallelSearchIndexArray.java
@@ -19,6 +19,37 @@ public class ParallelSearchIndexArray<T> extends RecursiveTask<Integer> {
         this.el = el;
     }
 
+    private static <T> int find(List<T> list, T el) {
+        int result = -1;
+        for (T t : list) {
+            if (el.equals(t)) {
+                result = list.indexOf(t);
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static <T> int findIndex(List<T> list, T el) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        return forkJoinPool.invoke(new ParallelSearchIndexArray<>(list, 0, list.size(), el));
+    }
+
+    @Override
+    protected Integer compute() {
+        if (list.size() < 10) {
+            return find(list, el);
+        }
+        int mid = (from + to) / 2;
+        ParallelSearchIndexArray<T> leftSearch = new ParallelSearchIndexArray<>(list, from, mid, el);
+        ParallelSearchIndexArray<T> rightSearch = new ParallelSearchIndexArray<>(list, mid + 1, to, el);
+        leftSearch.fork();
+        rightSearch.fork();
+        int leftE = leftSearch.join();
+        int rightE = rightSearch.join();
+        return rightE != -1 ? rightE : leftE;
+    }
+
     @Override
     public String toString() {
         return "ParallelSearchIndexArray{" + "list=" + list
@@ -26,51 +57,14 @@ public class ParallelSearchIndexArray<T> extends RecursiveTask<Integer> {
                 + to + ", el=" + el + '}';
     }
 
-    private static <T> T find(List<T> list, T el) {
-        T result = null;
-        for (T t : list) {
-            if (t.equals(el)) {
-                result = t;
-                break;
-            }
-        }
-        return result;
-    }
-
-    @Override
-    protected Integer compute() {
-        if (from == to) {
-            return from;
-        }
-        int mid = (from + to) / 2;
-        ParallelSearchIndexArray<T> left = new ParallelSearchIndexArray<>(
-                list, from, mid, findIndex(list, el));
-        ParallelSearchIndexArray<T> right = new ParallelSearchIndexArray<>(
-                list, mid + 1, list.size(), findIndex(list, el));
-        left.fork();
-        right.fork();
-        Integer rightE = right.join();
-        Integer leftE = left.join();
-        return rightE != -1 ? rightE : leftE;
-    }
-
-    public static <T> T findIndex(List<T> list, T el) {
-        T result = null;
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        if (list.size() < 10) {
-            result = find(list, el);
-        } else {
-            forkJoinPool.invoke(new ParallelSearchIndexArray<>(list, 0, list.size(), el));
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
         List<String> listInt = new ArrayList<>();
         listInt.add("Thread");
         listInt.add("Pool");
         ParallelSearchIndexArray<String> stringParallelSearchIndexArray =
-                new ParallelSearchIndexArray<>(listInt, 0, listInt.size(), "Pool");
+                new ParallelSearchIndexArray<>(listInt, 0, listInt.size(), "Thread");
         System.out.println(stringParallelSearchIndexArray);
+        System.out.println(findIndex(listInt, "Pool"));
+        System.out.println(findIndex(listInt, "Thread"));
     }
 }
